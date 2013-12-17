fc5a56a5f7ad7261df847ee791ace251b765ae6d
jaspreet <jsingh@infobosccoma.net>
2013-12-17 15:44:37 +0100

    Versio 2 del threads i runnable

--- Threads_I_Runnable_1/src/Test.java
+++ Threads_I_Runnable_1/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 		
 		T_Thread1 t1 = new T_Thread1("Thread 1");
 		T_Thread2 t2 = new T_Thread2("Thread 2");
+		//T_Thread t3 = new T_Thread3 ("prova");
 		
 		Thread r_T1 = new Thread(new R_Thread1("Runnable Thread_1"));
 		Thread r_T2 = new Thread(new R_Thread2("Runnable Thread_2"));
